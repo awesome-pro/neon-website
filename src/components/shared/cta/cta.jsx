c2f4e461edbc302641f072163008af3aed9a0cd6
abhinandan-verma <abhinandanverma551@gmail.com>
2024-08-26 12:57:14 +0530

    fix(hooks): cleaned unwanted files

--- src/components/shared/cta/cta.jsx
+++ src/components/shared/cta/cta.jsx
@@ -1,9 +1,9 @@
 import clsx from 'clsx';
 import PropTypes from 'prop-types';
 
+import { Vortex } from 'components/pages/home/hero/vortex';
 import Button from 'components/shared/button';
 import Container from 'components/shared/container';
-import RiveAnimation from 'components/shared/rive-animation';
 import LINKS from 'constants/links';
 
 const Cta = ({
@@ -18,43 +18,44 @@ const Cta = ({
 
   return (
     <section className={clsx('get-started relative overflow-hidden', className)}>
-      <RiveAnimation
-        className="absolute left-1/2 top-[60%] aspect-[1.87365] w-[1703px] -translate-x-1/2 -translate-y-1/2 xl:w-[1432px] lg:w-[1126px] sm:w-[818px]"
-        src="/animations/shared/cta.riv"
-        artboard="footer"
-        intersectionRootMargin="0px 0px 600px 0px"
-        animationRootMargin="0px 0px 300px 0px"
-      />
-      <Container
-        className="pointer-events-none flex flex-col items-center justify-center"
-        size="1100"
+      <Vortex
+        backgroundColor="black"
+        rangeY={800}
+        particleCount={500}
+        baseHue={120}
+        className="flex h-full w-full flex-col items-center justify-center py-4"
       >
-        <h2
-          className={clsx(
-            'relative text-center font-title text-[68px] font-medium leading-[0.9] -tracking-[0.03em] text-white',
-            'xl:text-[56px] xl:tracking-extra-tight lg:text-[44px] sm:text-[32px]'
-          )}
-          dangerouslySetInnerHTML={{ __html: title }}
-        />
-        {description && (
-          <p className="mt-3.5 text-center text-lg tracking-extra-tight text-gray-new-70 xl:mt-3 xl:max-w-[414px] lg:max-w-[338px] lg:text-base md:mt-2 md:max-w-[289px] md:text-sm">
-            {description}
-          </p>
-        )}
-        <Button
-          className={clsx(
-            'pointer-events-auto relative !font-semibold tracking-tighter',
-            buttonClassName
-          )}
-          theme="primary"
-          to={buttonUrl}
-          target={isExternal ? '_blank' : null}
-          rel={isExternal ? 'noopener noreferrer' : null}
-          tag_name="Footer"
+        <Container
+          className="pointer-events-none flex flex-col items-center justify-center"
+          size="1100"
         >
-          {buttonText}
-        </Button>
-      </Container>
+          <h2
+            className={clsx(
+              'relative text-center font-title text-[68px] font-medium leading-[0.9] -tracking-[0.03em] text-white',
+              'xl:text-[56px] xl:tracking-extra-tight lg:text-[44px] sm:text-[32px]'
+            )}
+            dangerouslySetInnerHTML={{ __html: title }}
+          />
+          {description && (
+            <p className="mt-3.5 text-center text-lg tracking-extra-tight text-gray-new-70 xl:mt-3 xl:max-w-[414px] lg:max-w-[338px] lg:text-base md:mt-2 md:max-w-[289px] md:text-sm">
+              {description}
+            </p>
+          )}
+          <Button
+            className={clsx(
+              'pointer-events-auto relative !font-semibold tracking-tighter',
+              buttonClassName
+            )}
+            theme="primary"
+            to={buttonUrl}
+            target={isExternal ? '_blank' : null}
+            rel={isExternal ? 'noopener noreferrer' : null}
+            tag_name="Footer"
+          >
+            {buttonText}
+          </Button>
+        </Container>
+      </Vortex>
     </section>
   );
 };
